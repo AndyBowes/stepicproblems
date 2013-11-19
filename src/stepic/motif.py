b07ad3b3daeffa7cb70de4b498be7ca062674c99
Andy Bowes <andy.bowes@bjss.com>
2013-11-19 16:16:58 +0000

    Randomised Motif Search

--- src/stepic/motif.py
+++ src/stepic/motif.py
@@ -4,6 +4,8 @@ Created on 16 Nov 2013
 @author: Andy Bowes
 '''
 from itertools import permutations, product
+from copy import copy
+from random import randint
 
 def getKmers(dna, k):
     """
@@ -22,10 +24,13 @@ def getKmerVariations(kmer, d):
         return [char]
     maskPattern = '1' * d + '0' * (len(kmer) - d)
     patterns = ["".join(p) for p in set(permutations(maskPattern))]
-    variations = [ ''.join(k) for p in patterns for k in product(*map(applyWildCard, kmer, p))]
+    variations = [''.join(k) for p in patterns for k in product(*map(applyWildCard, kmer, p))]
     return variations
 
 def motifEnumeration(dna, k, d):
+    """
+    Enumerate the kmers with up to d differences
+    """
     kmers = set([kmer for seq in dna for kmer in getKmers(seq, k) ])
     variants = sorted(set([v for kmer in kmers for v in getKmerVariations(kmer, d)]))
     for v in variants:
@@ -37,15 +42,26 @@ def motifEnumeration(dna, k, d):
             yield v
 
 def hammingDistance(seq1, seq2):
+    """
+    Calculate the Hamming Distance between 2 Sequences
+    """
     def checkChar(char1, char2):
-        if char1 == char2: return 0
+        if char1 == char2:
+            return 0
         return 1
     return sum([x for x in map(checkChar, seq1, seq2)])
 
 def bestScore(dna, kmer):
+    """
+    Find the minimum Hamming Distance for a kmer in a DNA Sequence
+    """
     return min([ hammingDistance(kmer, sub) for sub in getKmers(dna, len(kmer))])
 
 def medianString(dna, k):
+    """
+    Find the Median String in a list of DNA Sequences
+    This is the kmer with the lowest minimum Hamming Distance
+    """
     kmers = ["".join(p) for p in product('ACGT', repeat=k)]
     bestKmer = 'A' * k
     topScore = 200000
@@ -57,15 +73,21 @@ def medianString(dna, k):
     return bestKmer
 
 def kmerProb(kmer, profile):
+    """
+    Calculate the probability of a kmer given a Probility Profile
+    """
     prob = 1.0
     for i in range(len(kmer)):
         prob *= profile[i]['ACGT'.index(kmer[i])]
     return prob
 
 def probableKmer(dna, k, profile):
+    """
+    Find the most probable kmer in a dna sequence given a probability profile
+    """
     kmers = getKmers(dna, k)
     bestKmer = ''
-    bestProb = 0
+    bestProb = -1
     for kmer in kmers:
         prob = kmerProb(kmer, profile)
         if prob > bestProb:
@@ -74,46 +96,122 @@ def probableKmer(dna, k, profile):
     return bestKmer, bestProb
 
 def scoreMotifs(motifs):
-    occurs = [[0 for _ in xrange(4)] for _ in xrange(len(motifs))]
+    """
+    Calculate the Score for the supplied motifs
+    """
+    occurs = [[0 for _ in xrange(4)] for _ in xrange(len(motifs[0]))]
     noOfMotifs = len(motifs)
     for i in range(noOfMotifs):
         motif = motifs[i]
         for j in range(len(motif)):
-            occurs[j]['ACGT'.index(motif[j])] += 1
+            try:
+                occurs[j]['ACGT'.index(motif[j])] += 1
+            except IndexError as e:
+                raise e
     return sum([noOfMotifs - max([x for x in m]) for m in occurs ])
 
-def constructProbabilityProfile(motifs):
+def constructProbabilityProfile(motifs, pseudocount=False):
     """
     Construct the probability matrix for the set of Motifs
     """
-    occurs = [[0.0 for _ in xrange(4)] for _ in xrange(len(motifs))]
-    noOfMotifs = len(motifs)
-    for i in range(noOfMotifs):
+    noOfMotifs = len(motifs) + 4 if pseudocount else len(motifs)
+    defaultValue = 1.0 / noOfMotifs if pseudocount else 0.0
+    occurs = [[defaultValue for _ in xrange(4)] for _ in xrange(len(motifs[0]))]
+    for i in range(len(motifs)):
         motif = motifs[i]
         for j in range(len(motif)):
             occurs[j]['ACGT'.index(motif[j])] += (1.0 / noOfMotifs)
     return occurs
 
-def greedyMotifSearch(dna, k):
+def greedyMotifSearch(dna, k, pseudocount=False):
     """
     Perform a Greedy Search for the best kmers on a set of DNA
     """
     motifs = [seq[:k] for seq in dna]
-    bestMotifs = motifs
-    # profile = constructProbabilityProfile(motifs)
+    bestMotifs = copy(motifs)
     motifScore = scoreMotifs(motifs)
     for i in range(len(dna[0]) - k):
         motifs[0] = dna[0][i:i + k]
         for j in range(1, len(dna)):
-            profile = constructProbabilityProfile(motifs[:j])
+            profile = constructProbabilityProfile(motifs[:j], pseudocount)
             motifs[j], _ = probableKmer(dna[j], k, profile)
         score = scoreMotifs(motifs)
         if score < motifScore:
             motifScore = score
-            bestMotifs = motifs
+            bestMotifs = copy(motifs)
     return bestMotifs, motifScore
 
+def randomisedMotifSearch(dna, k, t):
+    """
+    Perform a Search stating with a random set of Motifs
+    """
+    def doSearch(dna, k):
+        motifs = [ '' for _ in range(len(dna))]
+        for i in xrange(len(dna)):
+            startPos = randint(0, len(dna[i]) - k)
+            motifs[i] = dna[i][startPos:startPos + k]
+        bestMotifs = copy(motifs)
+        motifScore = scoreMotifs(motifs)
+        while True:
+            profile = constructProbabilityProfile(motifs, True)
+            for j in range(len(dna)):
+                motifs[j], _ = probableKmer(dna[j], k, profile)
+            score = scoreMotifs(motifs)
+            if score < motifScore:
+                motifScore = score
+                bestMotifs = copy(motifs)
+            else:
+                break
+        return bestMotifs, motifScore
+    recordMotifs = []
+    recordScore = 10000
+    for _ in range(t):
+        iterMotifs, iterScore = doSearch(dna, k)
+        if iterScore < recordScore:
+            recordScore = iterScore
+            recordMotifs = iterMotifs
+    return recordMotifs, recordScore
+
+def gibbsSampling(dna, k, n, t):
+    """
+    Perform a Gibbs Sampling Search stating with a random set of Motifs
+    """
+    def doSearch(dna, k, n):
+        motifs = [ '' for _ in range(len(dna))]
+        for i in xrange(len(dna)):
+            startPos = randint(0, len(dna[i]) - k)
+            motifs[i] = dna[i][startPos:startPos + k]
+        bestMotifs = copy(motifs)
+        motifScore = scoreMotifs(motifs)
+        for _ in xrange(n):
+            i = randint(0, len(motifs) - 1)
+            profileMotifs = copy(motifs)
+            profileMotifs.pop(i)
+            profile = constructProbabilityProfile(profileMotifs, True)
+            motifs[i], _ = probableKmer(dna[i], k, profile)
+            score = scoreMotifs(motifs)
+            if score < motifScore:
+                motifScore = score
+                bestMotifs = copy(motifs)
+        return bestMotifs, motifScore
+    recordMotifs = []
+    recordScore = 10000
+    for _ in range(t):
+        iterMotifs, iterScore = doSearch(dna, k, n)
+        if iterScore < recordScore:
+            recordScore = iterScore
+            recordMotifs = iterMotifs
+    return recordMotifs, recordScore
+
+
 if __name__ == '__main__':
-    profile = constructProbabilityProfile(['CGTA', 'CAGA', 'TTAA', 'CACA'])
-    for l in profile:
-        print l
+#    prof = constructProbabilityProfile(['GACC'])
+#    for l in prof:
+#        print l
+    theMotifs, score = randomisedMotifSearch(['CGCCCCTCTCGGGGGTGTTCAGTAAACGGCCA',
+                                              'GGGCGAGGTATGTGTAAGTGCCAAGGTGCCAG',
+                                              'TAGTACCGAGACCGAAAGAAGTATACAGGCGT',
+                                              'TAGATCAAGTTTCAGGTGCACGTCGGTGAACC',
+                                              'AATCCACCAGCTCCACGTGCAATGTTGGCCTA'],
+                                              8, 500)
+    print theMotifs, score
--- testsrc/stepic/motif_test.py
+++ testsrc/stepic/motif_test.py
@@ -4,7 +4,7 @@ Created on 16 Nov 2013
 @author: andy
 '''
 import unittest
-from stepic.motif import motifEnumeration, medianString, probableKmer, greedyMotifSearch
+from stepic.motif import motifEnumeration, medianString, probableKmer, greedyMotifSearch, randomisedMotifSearch
 
 class MotifTest(unittest.TestCase):
 
@@ -44,9 +44,54 @@ class MotifTest(unittest.TestCase):
     def testGreedyMotifSearch(self):
         k = 3
         dna = ['GGCGTTCAGGCA', 'AAGAATCAGTCA', 'CAAGGAGTTCGC', 'CACGTCAATCAC', 'CAATAATATTCG']
-        bestMotifs = greedyMotifSearch(dna, k)
+        bestMotifs, _ = greedyMotifSearch(dna, k)
         self.assertEquals(bestMotifs, ['CAG', 'CAG', 'CAA', 'CAA', 'CAA'])
 
+    def testGreedyMotifSearchFromFile(self):
+        """
+        Greedy Motif Search from file
+        """
+        with open('data/greedyMotif.txt') as fp:
+            vals = [int(x) for x in fp.readline().split()]
+            k = vals[0]
+            dna = [seq for seq in fp.readlines()]
+            bestMotifs, _ = greedyMotifSearch(dna, k)
+            print 'Best Motifs'
+            for motif in bestMotifs:
+                print motif
+
+    def testGreedyMotifSearchWithPseudocount(self):
+        k = 3
+        dna = ['GGCGTTCAGGCA', 'AAGAATCAGTCA', 'CAAGGAGTTCGC', 'CACGTCAATCAC', 'CAATAATATTCG']
+        bestMotifs, _ = greedyMotifSearch(dna, k, True)
+        self.assertEquals(bestMotifs, ['TTC', 'ATC', 'TTC', 'ATC', 'TTC'])
+
+    def testGreedyMotifSearchWithPseudocountFromFile(self):
+        """
+        Greedy Motif Search from file
+        """
+        with open('data/greedyMotifPseudocount.txt') as fp:
+            vals = [int(x) for x in fp.readline().split()]
+            k = vals[0]
+            dna = [seq for seq in fp.readlines()]
+            bestMotifs, _ = greedyMotifSearch(dna, k, True)
+            print 'Best Motifs'
+            for motif in bestMotifs:
+                print motif
+
+    def testRandomisedMotifSearchFromFile(self):
+        """
+        Randomised Motif Search from file
+        """
+        with open('data/randomisedMotifSearch.txt') as fp:
+            vals = [int(x) for x in fp.readline().split()]
+            k = vals[0]
+            dna = [seq.strip() for seq in fp.readlines()]
+            bestMotifs, bestScore = randomisedMotifSearch(dna, k, 1000)
+            print 'Best Motifs'
+            for motif in bestMotifs:
+                print motif
+            print bestScore
 
 
 if __name__ == "__main__":
