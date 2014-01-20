db1d7274a829b4e545de22dabd0469b5679dccac
Andy Bowes <andy.bowes@bjss.com>
2014-01-20 08:43:53 +0000

    Burrow Wheeler - Pattern Matching Attempt 1

--- src/stepic/burrowwheeler.py
+++ src/stepic/burrowwheeler.py
@@ -2,6 +2,9 @@
 Created on 14 Jan 2014
 
 """
+from stepic.suffixarray import buildSuffixArray
+from itertools import chain, takewhile
+from collections import Counter
 
 def burrowWheelerTransform(sequence):
     """
@@ -29,8 +32,8 @@ def inverseBurrowWheelerTransform(sequence):
 def firstLast(bwt):
     mapping = sorted([(i, v) for i, v in enumerate(bwt)], key=lambda x: x[1])
     first = ''.join([m[1] for m in mapping])
-    lastToFirst = [m[0] for m in mapping]
-    print mapping
+    firstToLast = [m[0] for m in mapping]
+    lastToFirst = [x[0] for x in sorted([(i, v) for i, v in enumerate(firstToLast)], key=lambda y: y[1])]
     return first, lastToFirst
 
 def findPatterns(bwt, patterns):
@@ -39,9 +42,9 @@ def findPatterns(bwt, patterns):
     """
     first, lastToFirst = firstLast(bwt)
     for pattern in patterns:
-        yield bwmatching(first, bwt, pattern, lastToFirst)
+        yield bwmatching(pattern, bwt, lastToFirst)
 
-def bwmatching(firstColumn, lastColumn, pattern, lastToFirst):
+def bwmatching(pattern, lastColumn, lastToFirst):
     top = 0
     bottom = len(lastColumn)
     while top <= bottom:
@@ -49,24 +52,78 @@ def bwmatching(firstColumn, lastColumn, pattern, lastToFirst):
             symbol = pattern[-1]
             pattern = pattern[:-1]
             if symbol in lastColumn[top:bottom + 1]:
-                symbolIndexes = find(lastColumn[top:bottom + 1], symbol, top)
-                indexValues = [lastToFirst[i] for i in symbolIndexes]
-                print symbolIndexes
-                print indexValues
-                topIndex = min(indexValues)
-                bottomIndex = max(indexValues)
+                topIndex = lastColumn.index(symbol, top, bottom + 1)
+                bottomIndex = lastColumn.rindex(symbol, top, bottom + 1)
                 top = lastToFirst[topIndex]
                 bottom = lastToFirst[bottomIndex]
-                print topIndex, bottomIndex, lastColumn[topIndex], lastColumn[bottomIndex], top, bottom
             else:
-                return 0
-    return (bottom - top) + 1
+                return []
+        else:
+            break
+    return range(top, bottom + 1)
 
-def find(s, ch, offset):
-    return [offset + i for i, ltr in enumerate(s) if ltr == ch]
+def multiPatternMatching(sequence, patterns):
+    bwt = burrowWheelerTransform(sequence)
+    suffixArray = buildSuffixArray(sequence)
+
+    for match in findPatterns(bwt, patterns):
+        for pos in match:
+            yield suffixArray[pos]
+
+def patternMatchWithMismatches(sequence, patterns, maxMismatches):
+    """
+    Find locations of Patterns in the Sequence which have <= No of Mismatches
+    """
+    bwt = burrowWheelerTransform(sequence)
+    suffixArray = buildSuffixArray(sequence)
+    _, lastToFirst = firstLast(bwt)
+    for pattern in patterns:
+        fragmentIndexes = list(findSeedPositions(bwt, lastToFirst, pattern, maxMismatches))
+ #       candidatePos = Counter([suffixArray[y] - offset for offset, x in fragmentIndexes for y in x])
+ #       seedPositions = [cp[0] for cp in takewhile(lambda x : x[1] > 1, candidatePos.most_common())]
+ #       for pos in seedPositions:
+#            if pos in [14, 16, 57, 63, 69]:
+#                print "Missed : {0} : {1} : {2}".format(pos, pattern, sequence[pos:pos + len(pattern)])
+#            print "Found : {0} : {1}".format(pattern, pos)
+  #          if isSimilar(sequence[pos:pos + len(pattern)], pattern, maxMismatches):
+#                print "Match : {0} : {2} : {1}".format(pattern, sequence[pos:pos + len(pattern)], pos)
+   #             yield pos
+
+def findSeedPositions(bwt, lastToFirst, pattern, maxMismatches):
+    fragments = list(splitPattern(pattern, maxMismatches + 2))
+    for fragment in fragments:
+        yield fragment[0], bwmatching(fragment[1], bwt, lastToFirst)
+
+def splitPattern(pattern, n):
+    blockSize = (len(pattern) / n)
+    startPos = 0
+    while pattern:
+        yield startPos, pattern[:blockSize]
+        pattern = pattern[blockSize:]
+        startPos += blockSize
+
+def isSimilar(seq1, seq2, maxmismatches):
+    if (len(seq1) != len(seq2)):
+        return False
+    mismatches = 0
+    for i in range(len(seq1)):
+        if seq1[i] != seq2[i]:
+            mismatches += 1
+            if mismatches > maxmismatches:
+                break
+    else:
+        return True
+    return False
+
+def partialSuffixArray(sequence, freq):
+    return [ (i, v) for i, v in enumerate(buildSuffixArray(sequence)) if v % freq == 0 ]
+
 
 if __name__ == '__main__':  # pragma: no cover
-#    print burrowWheelerTransform('GCGTGCCTGGTCA$')
-#    print inverseBurrowWheelerTransform('enwvpeoseu$llt')
-    for o in findPatterns('TCCTCTATGAGATCCTATTCTATGAAACCTTCA$GACCAAAATTCTCCGGC', ['CCT', 'CAC', 'GAG', 'CAG', 'ATC']):
-        print o
+#    for o in findPatterns('TCCTCTATGAGATCCTATTCTATGAAACCTTCA$GACCAAAATTCTCCGGC', ['CCT', 'CAC', 'GAG', 'CAG', 'ATC']):
+#        print o
+    # print partialSuffixArray('PANAMABANANAS$', 5)
+#    for pos in multiPatternMatching('AATCGGGTTCAATCGGGGT$', ['ATCG', 'GGGT']):
+#        print pos
+#    print ', '.join([ "{0}:{1}".format(sp, block) for sp, block in splitPattern('ABCD' * 7, 3)])
+     print ' '.join([str(x) for x in patternMatchWithMismatches('ACATGCTACTTT$', ['ATT', 'GCC', 'GCTA', 'TATT'], 1)])
--- testsrc/stepic/burrowwheeler_test.py
+++ testsrc/stepic/burrowwheeler_test.py
@@ -4,9 +4,12 @@ Created on 15 Jan 2014
 
 """
 import unittest
+from cProfile import Profile
 
-from stepic.burrowwheeler import burrowWheelerTransform
+from stepic.burrowwheeler import burrowWheelerTransform, findPatterns, partialSuffixArray, \
+                                 multiPatternMatching, patternMatchWithMismatches
 
+from stepic.approximatematch import findApproximateMatches
 
 class Test(unittest.TestCase):
 
@@ -15,7 +18,39 @@ class Test(unittest.TestCase):
             sequences = [x.strip() for x in fp.readlines()]
             print burrowWheelerTransform(sequences[0])
 
+    def testBwtMatching(self):
+        with open('data/burrow/bwtmatching.txt') as fp:
+            bwt = fp.readline().strip()
+            patterns = fp.readline().strip().split(' ')
+            print ' '.join([str(x) for x in findPatterns(bwt, patterns)])
+
+    def testPartialSuffixArray(self):
+        with open('data/burrow/partialSuffixArray.txt') as fp:
+            sequence = fp.readline().strip()
+            freq = int(fp.readline().strip())
+            for i, v in partialSuffixArray(sequence, freq):
+                print str(i) + ',' + str(v)
+
+    def testMultiPatternMatching(self):
+        with open('data/burrow/multiPatternMatch.txt') as fp:
+            sequence = fp.readline().strip()
+            patterns = [l.strip() for l in fp.readlines()]
+            print ' '.join([str(pos) for pos in sorted(multiPatternMatching(sequence + '$', patterns))])
+
+    def testPatternMatchWithMismatches(self):
+        with open('data/burrow/mismatchPatternMatchLarge.txt') as fp:
+            sequence = fp.readline().strip()
+            patterns = fp.readline().strip().split(' ')
+#            print 'Patterns : {0}'.format(len(patterns))
+            mismatches = int(fp.readline().strip())
+            # print ' '.join([str(pos) for pos in sorted(patternMatchWithMismatches(sequence, patterns, mismatches))])
+            print len(patterns)
+            i = 0
+            for motif in patterns:
+                print i
+                findApproximateMatches(motif, mismatches, sequence)
+                i += 1
 
 if __name__ == "__main__":
-    # import sys;sys.argv = ['', 'Test.testName']
+    import sys;sys.argv = ['', 'Test.testPatternMatchWithMismatches']
     unittest.main()
