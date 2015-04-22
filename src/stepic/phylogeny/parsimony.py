96844fbcb1080c60cb8e0d8aea814543861e0290
Andy Bowes <andy.bowes@bjss.com>
2015-04-22 15:17:31 +0000

    Addition of calculation of ParsimonyScore

--- src/stepic/phylogeny/parsimony.py
+++ src/stepic/phylogeny/parsimony.py
@@ -5,6 +5,7 @@ Created on 19 Apr 2015
 '''
 
 import networkx as nx
+import random
 
 ALPHABET = 'ACGT'
 MAX_INFINITY = 999999999
@@ -17,8 +18,8 @@ def findRoot(graph):
     while len(graph.predecessors(nodeId)) > 0:
         nodeId = graph.predecessors(nodeId)[0]
     return nodeId
-    
-    
+
+
 def smallParsimony(graph):
     """
     Small Parsimony problem
@@ -27,27 +28,25 @@ def smallParsimony(graph):
     values of the internal nodes which give the smallest parsimony score.
     """
     rootNode = findRoot(graph)
-    
+
     calculateParsimonyMatrix(graph, rootNode)
-    
+
+    print "Parsimony Score:{0}".format(graph.node[rootNode]['parsimonyScore'])
+
     # Once the Parsimony Matrix has been calculated then recreate the DNA for each node
-    
+
     return graph
-    
+
 def calculateCombinedParsimony(childParsimony1, childParsimony2):
     """
     
     """
     def calculateElement(matrix1, matrix2):
-        return {char : min(v if k==char else v+1 for k,v in matrix1.iteritems()) 
-                        + min(v if k==char else v+1 for k,v in matrix2.iteritems()) for char in ALPHABET}
-    return [ calculateElement(childParsimony1[i], childParsimony2[i]) for i in range(len(childParsimony1))]
-        
-    
-    
-        
-    
-def calculateParsimonyMatrix(graph,nodeId):
+        return {char : min(v if k == char else v + 1 for k, v in matrix1.iteritems())
+                        + min(v if k == char else v + 1 for k, v in matrix2.iteritems()) for char in ALPHABET}
+    return map(calculateElement, childParsimony1, childParsimony2)
+
+def calculateParsimonyMatrix(graph, nodeId):
     """
     Calculate the Parsimony Matrix at a node.
     
@@ -66,16 +65,17 @@ def calculateParsimonyMatrix(graph,nodeId):
         childParsimony1 = calculateParsimonyMatrix(graph, childNodes[0])
         childParsimony2 = calculateParsimonyMatrix(graph, childNodes[1])
         parsimony = calculateCombinedParsimony(childParsimony1, childParsimony2)
-        
+
     graph.node[nodeId]['parsimony'] = parsimony
+    graph.node[nodeId]['parsimonyScore'] = sum((map(min, [x.itervalues() for x in parsimony])))
     return parsimony
-        
+
 def isLeaf(graph, nodeId):
     """
     Check if the given nodeId identifies a root node (i.e. it has no successors)
     """
-    return len(graph.successors(nodeId))==0
-    
+    return len(graph.successors(nodeId)) == 0
+
 def readParsimonyFile(parsimonyFile):
     """
     Extract the details from the ParsimonyFile 
@@ -86,22 +86,22 @@ def readParsimonyFile(parsimonyFile):
     for nodeId in range(numberOfLeaves):
         parentId, dna = parsimonyFile.readline().strip().split('->')
         graph.add_node(nodeId, {'dna':dna})
-        graph.add_edge(int(parentId),nodeId)
+        graph.add_edge(int(parentId), nodeId)
 
     for line in parsimonyFile.readlines():
         parentId, childId = map(int, line.strip().split('->'))
-        graph.add_edge(parentId,childId)
+        graph.add_edge(parentId, childId)
 
     return graph
 
 if __name__ == '__main__':
     with open('data/smallParsimony_extra.txt') as parsimonyFile:
         graph = readParsimonyFile(parsimonyFile)
-        
+
         for adjacency in graph.adjacency_iter():
-            for k,v in adjacency[1].items():
-                print '{0}->{1}'.format(adjacency[0],k)
+            for k, v in adjacency[1].items():
+                print '{0}->{1}'.format(adjacency[0], k)
 
         graph = smallParsimony(graph)
-        
+
         print "Finished"
--- src/stepic/phylogeny/phylogeny.py
+++ src/stepic/phylogeny/phylogeny.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import operator
 import re
-from itertools import ifilter, ifilterfalse, chain
+from itertools import ifilterfalse, chain
 from copy import deepcopy
 
 def buildGraph(edges):
@@ -9,7 +9,7 @@ def buildGraph(edges):
     Build a Graph from a list of edge triplet tuples
     """
     graph = nx.Graph()
-    for i,j,d in edges:
+    for i, j, d in edges:
         if i < j:
             graph.add_edge(i, j, {'weight':d})
     return graph
@@ -21,16 +21,16 @@ def readEdgesFile(edgesFile):
     pattern = re.compile(r"(\d*)->(\d*):(\d*)")
     for line in edgesFile:
         yield map(int, pattern.match(line).groups())
-        
+
 def generateDistanceMatrix(edgesFile):
     """
     Print the Distance Matrix as defined from the Edges in the input file
     """
-    edges = readEdgesFile(fp)   
+    edges = readEdgesFile(fp)
     graph = buildGraph(edges)
-    leafNodes = [node for node,degree in graph.degree_iter() if degree==1]
+    leafNodes = [node for node, degree in graph.degree_iter() if degree == 1]
     for i in leafNodes:
-        print '\t'.join([str(nx.shortest_path_length(graph,i,j,weight='weight')) for j in leafNodes])
+        print '\t'.join([str(nx.shortest_path_length(graph, i, j, weight='weight')) for j in leafNodes])
 
 
 def calculateLimbLength(node, distanceMatrix):
@@ -39,24 +39,24 @@ def calculateLimbLength(node, distanceMatrix):
     """
     def _innerCalc():
         l = len(distanceMatrix)
-        for i in ifilterfalse(lambda x: x==node,range(l-1)) :
-            for k in ifilterfalse(lambda x: x==node,range(i+1,l)):
-                #print "Inner Calc: {0} : {1}".format(i,k)
+        for i in ifilterfalse(lambda x: x == node, range(l - 1)) :
+            for k in ifilterfalse(lambda x: x == node, range(i + 1, l)):
+                # print "Inner Calc: {0} : {1}".format(i,k)
                 try:
                     yield ((distanceMatrix[i][node]
                             + distanceMatrix[k][node]
-                            - distanceMatrix[i][k])/2)
+                            - distanceMatrix[i][k]) / 2)
                 except IndexError as e:
-                    print "Index Error: {0} {1} {2}".format(i,k,node)
+                    print "Index Error: {0} {1} {2}".format(i, k, node)
                     raise e
     return min(_innerCalc())
 
 def findJoinPoint(node, distanceMatrix):
     l = len(distanceMatrix)
-    for i in ifilterfalse(lambda x: x==node,range(l-1)) :
-        for k in ifilterfalse(lambda x: x==node,range(i+1,l)):
+    for i in ifilterfalse(lambda x: x == node, range(l - 1)) :
+        for k in ifilterfalse(lambda x: x == node, range(i + 1, l)):
             if (distanceMatrix[node][i] + distanceMatrix[node][k]) == distanceMatrix[i][k]:
-                return i,k
+                return i, k
 
 def getLimbLength(limbLengthFile):
     node = int(limbLengthFile.readline())
@@ -66,7 +66,7 @@ def getLimbLength(limbLengthFile):
 def removeEntry(distanceMatrix, index):
     """
     """
-    newMatrix = [list(chain(x[:index],x[index+1:])) for x in distanceMatrix]
+    newMatrix = [list(chain(x[:index], x[index + 1:])) for x in distanceMatrix]
     del newMatrix[index]
     return newMatrix
 
@@ -80,11 +80,11 @@ def additivePhylogenyFromFile(distanceMatrixFile, separator='\t'):
     nextNode = len(distanceMatrix)
 
     _, graph = additivePhylogeny(nextNode, nodeIds, distanceMatrix)
-   
-    #graph = reconstructGraph(distanceMatrix)
+
+    # graph = reconstructGraph(distanceMatrix)
     for adjacency in graph.adjacency_iter():
-        for k,v in adjacency[1].items():
-            yield adjacency[0],k, v['weight']
+        for k, v in adjacency[1].items():
+            yield adjacency[0], k, v['weight']
 
 def additivePhylogeny(nextNode, nodeIds, distanceMatrix):
     """
@@ -95,42 +95,42 @@ def additivePhylogeny(nextNode, nodeIds, distanceMatrix):
         graph.add_edge(nodeIds[0], nodeIds[1], {'weight':distanceMatrix[0][1]})
         return nextNode, graph
 
-    #print "Distance Matrix: {0}".format(distanceMatrix)
+    # print "Distance Matrix: {0}".format(distanceMatrix)
     limbLengths = [calculateLimbLength(i, distanceMatrix) for i in range(len(nodeIds))]
     minIndex, minLimbLength = min(enumerate(limbLengths), key=operator.itemgetter(1))
 
     leafNodeId = nodeIds[minIndex]
-    
+
     baldMatrix = deepcopy(distanceMatrix)
     # Adjust the remaining lengths in the column, row
     baldMatrix[minIndex] = [x - minLimbLength for x in baldMatrix[minIndex]]
-    for i in ifilterfalse(lambda x:x==minIndex, range(len(nodeIds))):
+    for i in ifilterfalse(lambda x:x == minIndex, range(len(nodeIds))):
         baldMatrix[i][minIndex] -= minLimbLength
 
     nextNodes = deepcopy(nodeIds)
     del nextNodes[minIndex]
     nextNode, graph = additivePhylogeny(nextNode, nextNodes, removeEntry(distanceMatrix, minIndex))
-    
+
     # Attach the leaf node at the appropriate point in the returned graph
-    i,k = findJoinPoint(minIndex, baldMatrix)
+    i, k = findJoinPoint(minIndex, baldMatrix)
     path = nx.shortest_path(graph, nodeIds[i], nodeIds[k], weight="weight")
     limbLength2 = baldMatrix[i][minIndex]
     i = 0
-    while graph[path[i]][path[i+1]]['weight'] < limbLength2:
-        limbLength2 -= graph[path[i]][path[i+1]]['weight']
+    while graph[path[i]][path[i + 1]]['weight'] < limbLength2:
+        limbLength2 -= graph[path[i]][path[i + 1]]['weight']
         i += 1
     if limbLength2 == 0:
         graph.add_edge(path[i], leafNodeId, {'weight':minLimbLength})
     else:
-        totalLength = graph[path[i]][path[i+1]]['weight']
-        graph.add_edge(path[i],nextNode,{'weight':limbLength2})
-        graph.add_edge(path[i+1],nextNode,{'weight':totalLength-limbLength2})
+        totalLength = graph[path[i]][path[i + 1]]['weight']
+        graph.add_edge(path[i], nextNode, {'weight':limbLength2})
+        graph.add_edge(path[i + 1], nextNode, {'weight':totalLength - limbLength2})
         graph.add_edge(nextNode, leafNodeId, {'weight':minLimbLength})
-        graph.remove_edge(path[i], path[i+1])
+        graph.remove_edge(path[i], path[i + 1])
         nextNode += 1
-    
+
     return nextNode, graph
-    
+
 if __name__ == '__main__':
 #    with open("data/edges.txt") as fp:
 #        generateDistanceMatrix(fp)
@@ -138,5 +138,5 @@ if __name__ == '__main__':
 #    with open("data/limbLength.txt") as fp:
 #        print getLimbLength(fp)
     with open("data/additivePhylogeny.txt") as fp:
-        for i,j,w in additivePhylogenyFromFile(fp, separator=' '):
-            print "{0}->{1}:{2}".format(i,j,w)
+        for i, j, w in additivePhylogenyFromFile(fp, separator=' '):
+            print "{0}->{1}:{2}".format(i, j, w)
